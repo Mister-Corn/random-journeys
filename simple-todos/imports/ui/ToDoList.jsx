a752736f9b3e18ed107cb94d62a2044c9cc31c7d
Mister-Corn <code@ronaldlibago.com>
2019-02-10 16:07:51 -0800

    simple-todos: Wire up ToDoList w/ Tasks collection

--- simple-todos/imports/ui/ToDoList.jsx
+++ simple-todos/imports/ui/ToDoList.jsx
@@ -1,20 +1,28 @@
-import React from "react";
-import { Task } from "./Task";
+import React from 'react';
+import { withTracker } from 'meteor/react-meteor-data';
+import { Task } from './Task';
+import { Tasks } from '../api/tasks';
 
-export const ToDoList = () => {
+const ToDoList = (props) => {
   const getTasks = () => {
     return [
-      { _id: 1, text: "This is task 1" },
-      { _id: 2, text: "This is task 2" },
-      { _id: 3, text: "This is task 3" }
+      { _id: 1, text: 'This is task 1' },
+      { _id: 2, text: 'This is task 2' },
+      { _id: 3, text: 'This is task 3' },
     ];
   };
 
   return (
     <ul>
-      {getTasks().map(task => (
+      {props.tasks.map((task) => (
         <Task key={task._id} task={task} />
       ))}
     </ul>
   );
 };
+
+export default withTracker(() => {
+  return {
+    tasks: Tasks.find({}).fetch(),
+  };
+})(ToDoList);
