7830ac574ff92b2af567c4bdbb9e58bd0bd91d0d
Mister-Corn <code@ronaldlibago.com>
2019-02-10 16:39:51 -0800

    simple-todos: Add check toggle and delete to li items

--- simple-todos/imports/ui/Task.jsx
+++ simple-todos/imports/ui/Task.jsx
@@ -1,3 +1,31 @@
-import React from "react";
+import React from 'react';
+import { Tasks } from '../api/tasks';
 
-export const Task = props => <li>{props.task.text}</li>;
+export const Task = (props) => {
+  const toggleChecked = () => {
+    Tasks.update(props.task._id, {
+      $set: { checked: !props.task.checked },
+    });
+  };
+
+  const deleteThisTask = () => {
+    Tasks.remove(props.task._id);
+  };
+
+  return (
+    <li>
+      <button className='delete' onClick={deleteThisTask}>
+        &times;
+      </button>
+
+      <input
+        type='checkbox'
+        readOnly
+        checked={Boolean(props.task.checked)}
+        onClick={toggleChecked}
+      />
+
+      <span className='text'>{props.task.text}</span>
+    </li>
+  );
+};
