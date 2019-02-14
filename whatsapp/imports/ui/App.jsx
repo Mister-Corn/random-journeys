22b58aabe0cfad4be0c0d0b28f95cd19ac31debe
mister-corn <code@ronaldlibago.com>
2019-02-14 13:33:27 -0800

    whatsapp: Set up view stubs

--- whatsapp/imports/ui/App.jsx
+++ whatsapp/imports/ui/App.jsx
@@ -1,12 +1,19 @@
-import React from 'react';
-import Hello from './Hello.jsx';
-import Info from './Info.jsx';
+import React from "react";
+// Components
+import { Link, Router } from "@reach/router";
+import Hello from "./Hello.jsx";
+import Info from "./Info.jsx";
 
 const App = () => (
   <div>
-    <h1>Welcome to Meteor!</h1>
-    <Hello />
-    <Info />
+    <h1>WhatsApp Clone</h1>
+    <div className="links">
+      <Link to="/favorites">Favorites</Link>
+      <Link to="/recents">Recents</Link>
+      <Link to="/contacts">Contacts</Link>
+      <Link to="/chats">Chats</Link>
+      <Link to="/settings">settings</Link>
+    </div>
   </div>
 );
 
