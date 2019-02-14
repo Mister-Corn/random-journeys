d1181043b0663477be4fe40b7930fcb4b791a6d3
mister-corn <code@ronaldlibago.com>
2019-02-14 13:40:09 -0800

    whatsapp: Set up Links and Router

--- whatsapp/imports/ui/App.jsx
+++ whatsapp/imports/ui/App.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 // Components
 import { Link, Router } from "@reach/router";
-import Hello from "./Hello.jsx";
-import Info from "./Info.jsx";
+import Favorites from "./Favorites";
+import Recents from "./Recents";
+import Contacts from "./Contacts";
+import Chats from "./Chats";
+import Settings from "./Settings";
 
 const App = () => (
   <div>
@@ -12,8 +15,15 @@ const App = () => (
       <Link to="/recents">Recents</Link>
       <Link to="/contacts">Contacts</Link>
       <Link to="/chats">Chats</Link>
-      <Link to="/settings">settings</Link>
+      <Link to="/settings">Settings</Link>
     </div>
+    <Router>
+      <Favorites path="/favorites" />
+      <Recents path="/recents" />
+      <Contacts path="/contacts" />
+      <Chats path="/chats" />
+      <Settings path="/settings" />
+    </Router>
   </div>
 );
 
