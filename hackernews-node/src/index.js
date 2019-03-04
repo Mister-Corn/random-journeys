27331a98d77d7cf6e5c86e45c8e030ad2bb95169
Ronald Libago <code@ronaldlibago.com>
2019-03-04 15:18:44 -0800

    graphql: Set up findById and update defs/resolvers

--- hackernews-node/src/index.js
+++ hackernews-node/src/index.js
@@ -1,22 +1,5 @@
 const { GraphQLServer } = require('graphql-yoga');
 
-const typeDefs = `
-type Query {
-    info: String!
-    feed: [Link!]!
-}
-
-type Link {
-    id: ID!
-    description: String!
-    url: String!
-}
-
-type Mutation {
-    post(url: String!, description: String!): Link!
-}
-`;
-
 // Static Dummy Data to Start
 let links = [
   {
@@ -26,11 +9,43 @@ let links = [
   }
 ];
 
+let idCount = links.length;
+
 const resolvers = {
   Query: {
     info: () => `This is the API for the GraphQL Tutorial`,
-    feed: () => links
+    feed: () => links,
+    link: (parents, args) => {
+      const result = links.filter(link => link.id === args.id);
+      return result.length ? result[0] : null;
+    }
+  },
+  Mutation: {
+    post: (parents, args) => {
+      const link = {
+        id: `link-${idCount}`,
+        description: args.description,
+        url: args.url
+      };
+      links.push(link);
+      return link;
+    },
+    updateLink: (parents, args) => {
+      let updatedLink;
+      links = links.map(link => {
+        if (link.id === args.id) {
+          updatedLink = { ...link, ...args };
+          return updatedLink;
+        }
+        return link;
+      });
+      return updatedLink;
+    }
   }
+  // If your object key/value pairs map vis-a-vis with the type def,
+  // the below isn't necessary: GraphQL/Prisma can figure it out
+  // However, know the below is useful for more complex data shapes
+  // or if you need to process the data before sending it to the client
   //   Link: {
   //       id: (parent) => parent.id,
   //       description: (parent) => parent.description,
@@ -39,7 +54,7 @@ const resolvers = {
 };
 
 const server = new GraphQLServer({
-  typeDefs,
+  typeDefs: './src/schema.graphql',
   resolvers
 });
 
