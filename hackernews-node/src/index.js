ac97b4aef06c42402fe2393c3598cdd62174b3fc
Ronald Libago <code@ronaldlibago.com>
2019-03-04 14:19:44 -0800

    graphql: Set up dummy data and def/resolver for it

--- hackernews-node/src/index.js
+++ hackernews-node/src/index.js
@@ -1,20 +1,42 @@
-const { GraphQLServer } = require('graphql-yoga');
+const { GraphQLServer } = require("graphql-yoga");
 
 const typeDefs = `
 type Query {
     info: String!
+    feed: [Link!]!
+}
+
+type Link {
+    id: ID!
+    description: String!
+    url: String!
 }
 `;
 
+// Static Dummy Data to Start
+let links = [
+  {
+    id: "link-0",
+    url: "www.howtographql.com",
+    description: "Fullstack tutorial for GraphQL"
+  }
+];
+
 const resolvers = {
-    Query: {
-        info: () => `This is the API for the GraphQL Tutorial`
-    }
+  Query: {
+    info: () => `This is the API for the GraphQL Tutorial`,
+    feed: () => links,
+  },
+//   Link: {
+//       id: (parent) => parent.id,
+//       description: (parent) => parent.description,
+//       url: (parent) => parent.url,
+//   }
 };
 
 const server = new GraphQLServer({
-    typeDefs,
-    resolvers,
+  typeDefs,
+  resolvers
 });
 
 server.start(() => console.log(`Server is running on http://localhost:4000`));
