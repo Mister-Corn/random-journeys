4c532522d813dd2d12065a1576e103ec36f0ced0
Ronald Libago <code@ronaldlibago.com>
2019-03-04 16:52:21 -0800

    graphql: Add various defs for Users for both Prisma and server

--- hackernews-node/src/index.js
+++ hackernews-node/src/index.js
@@ -1,58 +1,47 @@
 const { GraphQLServer } = require('graphql-yoga');
-
-// Static Dummy Data to Start
-let links = [
-  {
-    id: 'link-0',
-    url: 'www.howtographql.com',
-    description: 'Fullstack tutorial for GraphQL'
-  }
-];
-
-let idCount = links.length;
+const { prisma } = require('./generated/prisma-client');
 
 const resolvers = {
   Query: {
     info: () => `This is the API for the GraphQL Tutorial`,
-    feed: () => links,
-    link: (parents, args) => {
-      const result = links.filter(link => link.id === args.id);
-      return result.length ? result[0] : null;
-    }
+    feed: (root, args, context, info) => {
+      return context.prisma.links();
+    },
+    // below not working
+    link: (root, args, context, info) => {
+      return context.prisma.link(args.id);
+    },
   },
   Mutation: {
-    post: (parents, args) => {
-      const link = {
-        id: `link-${idCount}`,
+    post: (root, args, context) => {
+      return context.prisma.createLink({
+        url: args.url,
         description: args.description,
-        url: args.url
-      };
-      links.push(link);
-      return link;
-    },
-    updateLink: (parents, args) => {
-      let updatedLink;
-      links = links.map(link => {
-        if (link.id === args.id) {
-          updatedLink = { ...link, ...args };
-          return updatedLink;
-        }
-        return link;
       });
-      return updatedLink;
     },
-    deleteLink: (parents, args) => {
-      let deletedLink;
-      links = links.filter(link => {
-        const match = link.id === args.id;
-        if (match) {
-          deletedLink = link;
-        }
-        return !match;
-      });
-      return deletedLink;
-    }
-  }
+    // updateLink: (parents, args) => {
+    //   let updatedLink;
+    //   links = links.map(link => {
+    //     if (link.id === args.id) {
+    //       updatedLink = { ...link, ...args };
+    //       return updatedLink;
+    //     }
+    //     return link;
+    //   });
+    //   return updatedLink;
+    // },
+    // deleteLink: (parents, args) => {
+    //   let deletedLink;
+    //   links = links.filter(link => {
+    //     const match = link.id === args.id;
+    //     if (match) {
+    //       deletedLink = link;
+    //     }
+    //     return !match;
+    //   });
+    //   return deletedLink;
+    // }
+  },
   // If your object key/value pairs map vis-a-vis with the type def,
   // the below isn't necessary: GraphQL/Prisma can figure it out
   // However, know the below is useful for more complex data shapes
@@ -66,7 +55,8 @@ const resolvers = {
 
 const server = new GraphQLServer({
   typeDefs: './src/schema.graphql',
-  resolvers
+  resolvers,
+  context: { prisma },
 });
 
 server.start(() => console.log(`Server is running on http://localhost:4000`));
